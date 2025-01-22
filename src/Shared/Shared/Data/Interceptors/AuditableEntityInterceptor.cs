70b3b6d0d0707ea1af59f1c264744e4a142ae833
AhmedNourElnabi <91994457+nourelnaby4@users.noreply.github.com>
2025-01-22 16:54:26 +0200

    feate: Develop AuditableEntityInterceptor and DispatchDomainEventsInterceptor To Trigger  Domain Event Handlers

---
+++ src/Shared/Shared/Data/Interceptors/AuditableEntityInterceptor.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Shared.DDD;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.ComponentModel.Design;
+
+namespace Shared.Data.Interceptors;
+public class AuditableEntityInterceptor : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        UpdateEntities(eventData.Context);
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        UpdateEntities(eventData.Context);
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private void UpdateEntities(DbContext? context)
+    {
+        if (context == null) return;
+
+        foreach (var entry in context.ChangeTracker.Entries<IEntity>())
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedBy = "ahmed";
+                entry.Entity.CreatedAt = DateTime.UtcNow;
+            }
+
+            if (entry.State == EntityState.Added || entry.State == EntityState.Modified || entry.HasChangedOwnedEntities())
+            {
+                entry.Entity.LastModifiedBy = "ahmed";
+                entry.Entity.LastModified = DateTime.UtcNow;
+            }
+        }
+    }
+}
+
+public static class Extensions
+{
+    //The HasChangedOwnedEntities extension method is designed to determine whether any owned entities related to the given EntityEntry have been added or modified
+    public static bool HasChangedOwnedEntities(this EntityEntry entry) =>
+        entry.References.Any(r =>
+            r.TargetEntry != null &&
+            r.TargetEntry.Metadata.IsOwned() &&
+            (r.TargetEntry.State == EntityState.Added || r.TargetEntry.State == EntityState.Modified));
+}
