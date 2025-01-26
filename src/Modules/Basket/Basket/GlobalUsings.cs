bf0cca99f4645296f288ca244c39749c3ef3a73b
AhmedNourElnabi <91994457+nourelnaby4@users.noreply.github.com>
2025-01-26 14:44:32 +0200

    Develop Basket Data-Infrastructure with EF Core Code First

---
+++ src/Modules/Basket/Basket/GlobalUsings.cs
@@ -0,0 +1,19 @@
+﻿global using Shared.DDD;
+global using Shared.Data.Seed;
+global using Shared.Data;
+global using Shared.CQRS;
+global using Shared.Pagination;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Routing;
+global using Microsoft.AspNetCore.Builder;
+global using Mapster;
+global using MediatR;
+global using Carter;
+global using FluentValidation;
+global using Basket.Basket.Models;
+global using Basket.Data;
+
+
